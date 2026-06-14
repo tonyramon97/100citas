680fee07046ca216db23cc1afdc6e5adeda3c080
Anthony Ramon Gonzalez <135547195+tonyramon97@users.noreply.github.com>
2026-06-13 22:17:26 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,43 @@
-const CACHE_NAME = '100citas-v1';
-const ASSETS = ['./index.html', './manifest.json'];
+const CACHE_NAME = '100citas-v2';
+const SHELL = [
+  './index.html',
+  './manifest.json',
+  './portada.png'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(SHELL)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
+  const { request } = e;
+  if (request.method !== 'GET') return;
+
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
+    caches.match(request).then(cached => {
+      const fetchAndUpdate = fetch(request).then(res => {
+        if (res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then(c => c.put(request, clone));
+        }
+        return res;
+      }).catch(() => cached);
+
+      if (cached) {
+        fetchAndUpdate.catch(() => {});
+        return cached;
+      }
+      return fetchAndUpdate.catch(() => caches.match('./index.html'));
+    })
   );
 });
